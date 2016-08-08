661fc681cce17ffec4bfd93eeb50283a95b8c409
lanzheng <lanzheng@pku.edu.cn>
2016-08-09 00:37:35 +0800

    216

---
+++ round3review/216. Combination Sum III..cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+    vector<vector<int>> ret;
+    vector<int> temp;
+    vector<vector<int>> combinationSum3(int k, int n) {
+        dfs(1, k, n);
+        return ret;
+    }
+    
+    void dfs(int start, int k, int n) {
+        if(k == 0 && n == 0) {
+            ret.push_back(temp);
+            return;
+        }
+        if(k == 0 || n == 0)
+            return;
+        for(int i = start; i <= 10 - k && i <= n; i ++) {
+            temp.push_back(i);
+            dfs(i + 1, k - 1, n - i);
+            temp.pop_back();
+        }
+    }
+};
